2e15006c82c58880c4313a527b03adc96eb1c2bb
soons <soonso00@naver.com>
2022-06-03 12:47:16 +0900

    make database

--- app/src/main/java/org/isfp/housemate/MainActivity.java
+++ app/src/main/java/org/isfp/housemate/MainActivity.java
@@ -2,7 +2,11 @@ package org.isfp.housemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.appsearch.GetByDocumentIdRequest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signInButton = (Button) findViewById(R.id.signInButton);
+        Button signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/isfp/housemate/SignUpActivity.java
@@ -0,0 +1,68 @@
+package org.isfp.housemate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+    //String tempKey;
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/inspiration");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference userRef = database.getReference("user");
+
+        //initView();
+    }
+
+    public void signup(View view){
+        EditText inputEmail = (EditText) findViewById(R.id.inputEmail);
+        EditText inputPW = (EditText) findViewById(R.id.inputPW);
+        final String email = inputEmail.getText().toString();
+        final String password = inputPW.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()){ return;}
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put("email", email);
+        dataToSave.put("password", password);
+        mDocRef.set(dataToSave).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignUpActivity.this, "회원가입 완료", Toast.LENGTH_SHORT).show();
+                    Log.d("signup", "회원가입 성공");
+                } else{
+                    Log.w("signup", "회원가입 실패", task.getException());
+                }
+            }
+        });
+
+        Intent intent = new Intent(getApplicationContext(), ConnectActivity.class);
+        startActivity(intent);
+    }
+}
