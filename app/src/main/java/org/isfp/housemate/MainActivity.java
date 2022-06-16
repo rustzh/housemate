54fdea76623a48d118b3c21a5b1b4fd5c8d7e38b
hyunji <vbcnck55@gmail.com>
2022-06-16 21:12:18 +0900

    ad

--- app/src/main/java/org/isfp/housemate/MainActivity.java
+++ app/src/main/java/org/isfp/housemate/MainActivity.java
@@ -2,6 +2,8 @@ package org.isfp.housemate;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.appsearch.GetByDocumentIdRequest;
 import android.content.Intent;
@@ -19,7 +21,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
@@ -28,23 +33,26 @@ public class MainActivity extends AppCompatActivity {
 
     TextView monthYearText;//년월 텍스트뷰
     LocalDate selectedDate;
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        monthYearText=findViewById(R.id.monthYearText);
-        ImageButton prevBtn=findViewById(R.id.pre_btn);
-        ImageButton nextBtn=findViewById(R.id.next_btn);
+        monthYearText = findViewById(R.id.monthYearText);
+        ImageButton prevBtn = findViewById(R.id.pre_btn);
+        ImageButton nextBtn = findViewById(R.id.next_btn);
+        recyclerView=findViewById(R.id.recyclerView);
         //현재 날짜
-        selectedDate=LocalDate.now();
+        selectedDate = LocalDate.now();
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance("https://housemate-6fa71-default-rtdb.firebaseio.com/");
         System.out.println("getCurrentUSer = " + auth.getCurrentUser());
 
-        if (auth.getCurrentUser() == null){
+        if (auth.getCurrentUser() == null) {
             System.out.println("go to sign in");
             Intent intent = new Intent(MainActivity.this, SignInAndUpActivity.class);
             startActivity(intent);
@@ -55,29 +63,53 @@ public class MainActivity extends AppCompatActivity {
         prevBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectedDate=selectedDate.minusMonths(1);
+                selectedDate = selectedDate.minusMonths(1);
                 setMonthView();
             }
         });//이전달 눌렀을때
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectedDate=selectedDate.plusMonths(1);
+                selectedDate = selectedDate.plusMonths(1);
                 setMonthView();
             }
         });//다음달 눌렀을때
     }
-    private String monthYearFromDate(LocalDate date){
-        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("MM월 yyyy");
+
+    private String monthYearFromDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM월 yyyy");
         return date.format(formatter);
     }
 
     private void setMonthView() {
         monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> dayList=daysInMonthList(selectedDate);
+        CalendarAdapter adapter=new CalendarAdapter(dayList);
+        RecyclerView.LayoutManager manager=new GridLayoutManager(getApplicationContext(),7);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private ArrayList<String> daysInMonthList(LocalDate date) {
+        ArrayList<String> dayList = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+        int lastDay = yearMonth.lengthOfMonth();
+        LocalDate firstDay = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstDay.getDayOfWeek().getValue();
+
+        for (int i = 1; i < 42; i++) {
+            if (i <= dayOfWeek || i > lastDay + dayOfWeek) {
+                dayList.add("");
+
+            } else {
+                dayList.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return dayList;
+
     }
 
 }
-
 // 로그아웃 버튼
 //    Button logOutButton = (Button) findViewById(R.id.logOutButton);
 //        logOutButton.setOnClickListener(new View.OnClickListener(){
