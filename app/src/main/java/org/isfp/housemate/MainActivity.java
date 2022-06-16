213a58c2e9eaa69b6404c2fa807ac87ddc844778
hyunji <vbcnck55@gmail.com>
2022-06-16 20:24:33 +0900

    df

--- app/src/main/java/org/isfp/housemate/MainActivity.java
+++ app/src/main/java/org/isfp/housemate/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -16,15 +18,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase database;
     Button logOutButton;
 
+    TextView monthYearText;//년월 텍스트뷰
+    LocalDate selectedDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        monthYearText=findViewById(R.id.monthYearText);
+        ImageButton prevBtn=findViewById(R.id.pre_btn);
+        ImageButton nextBtn=findViewById(R.id.next_btn);
+        //현재 날짜
+        selectedDate=LocalDate.now();
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance("https://housemate-6fa71-default-rtdb.firebaseio.com/");
@@ -36,14 +50,41 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
-        Button logOutButton = (Button) findViewById(R.id.logOutButton);
-        logOutButton.setOnClickListener(new View.OnClickListener(){
+        setMonthView();//화면설정
+
+        prevBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                auth.signOut();
-                Intent intent = new Intent(getApplicationContext(), SignInAndUpActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                selectedDate=selectedDate.minusMonths(1);
+                setMonthView();
             }
-        });
+        });//이전달 눌렀을때
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedDate=selectedDate.plusMonths(1);
+                setMonthView();
+            }
+        });//다음달 눌렀을때
     }
+    private String monthYearFromDate(LocalDate date){
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("MM월 yyyy");
+        return date.format(formatter);
+    }
+
+    private void setMonthView() {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+    }
+
 }
+
+// 로그아웃 버튼
+//    Button logOutButton = (Button) findViewById(R.id.logOutButton);
+//        logOutButton.setOnClickListener(new View.OnClickListener(){
+//@Override
+//public void onClick(View view){
+//        auth.signOut();
+//        Intent intent = new Intent(getApplicationContext(), SignInAndUpActivity.class);
+//        startActivity(intent);
+//        }
+//        });
