6e178fbbead2a2730305c5c90aa3eb3c07ffcd59
hyunji <vbcnck55@gmail.com>
2022-06-18 10:58:51 +0900

    work

--- app/src/main/java/org/isfp/housemate/Listdata.java
+++ app/src/main/java/org/isfp/housemate/Listdata.java
@@ -1,2 +1,9 @@
-package org.isfp.housemate;public class listdata {
+package org.isfp.housemate;
+
+public class Listdata {
+    private String housework;
+    public void setWork(String housework){this.housework = housework;}
+    public String getWork(){return this.housework;}
+
 }
+//adapter에서 각 아이템에 대한 데이터를 저장할 객체가 필요
